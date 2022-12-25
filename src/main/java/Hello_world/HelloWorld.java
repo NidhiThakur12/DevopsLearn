61ef0c2fe2e7ea7816f261c9956a834c062f01d3
nidhithakur <nthakur@tillster.com>
2022-12-25 23:14:52 +0530

    added new lines in java file

--- src/main/java/Hello_world/HelloWorld.java
+++ src/main/java/Hello_world/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 
 public static void main(String [] args) {
 	System.out.println("Hello World");
+	System.out.println("This is my first devops program");
+	System.out.println("Thank you");
 }
 }
